83da7bde8cdfc3634d538f19a9fa1c9e8e099a84
Vaerus <drakocha-1-2@mail.ru>
2022-03-11 11:53:23 +0300

    lab2

---
+++ Task_2/Task_2/PeopleContext.cs
@@ -0,0 +1,19 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_2
+{
+    class PeopleContext : DbContext
+    {
+        public PeopleContext() : base("MyConnection")
+        {
+
+        }
+        public DbSet<Person> Persons { get; set; }
+    }
+}
